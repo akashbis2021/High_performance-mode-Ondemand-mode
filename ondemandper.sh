845ee60619d74314dd7d326c51cc80683f945558
Akash Biswas INDIAN <96134576+akashbis2021@users.noreply.github.com>
2023-03-02 20:59:24 +0530

    Add files via upload
    High-performance help's to get increase the frequency of the core.
    On-demand-performance help's to get normal frequency of the core .. which regulate automatically due to the work load.

---
+++ ondemandper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo cpufreq-set -c 0 -g ondemand
+sudo cpufreq-set -c 1 -g ondemand
+sudo cpufreq-set -c 2 -g ondemand
+sudo cpufreq-set -c 3 -g ondemand
+sudo cpufreq-set -c 4 -g ondemand
+sudo cpufreq-set -c 5 -g ondemand
+sudo cpufreq-set -c 6 -g ondemand
+sudo cpufreq-set -c 7 -g ondemand
